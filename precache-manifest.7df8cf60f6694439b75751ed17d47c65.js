6c9ac0c5afe361873071c28e949f36d825f2e723
Roman <romikkus@gmail.com>
2020-09-30 11:51:11 +0300

    Updates

---
+++ precache-manifest.7df8cf60f6694439b75751ed17d47c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a824e1963301fff0738470cec6bbf2f8",
+    "url": "/grocery-store/index.html"
+  },
+  {
+    "revision": "194cd17ec83138eae62c",
+    "url": "/grocery-store/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df6f06eac2543331cec0",
+    "url": "/grocery-store/static/js/2.9fd7190a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-store/static/js/2.9fd7190a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194cd17ec83138eae62c",
+    "url": "/grocery-store/static/js/main.ccbd1b97.chunk.js"
+  },
+  {
+    "revision": "1ae08186275a7b70cd22",
+    "url": "/grocery-store/static/js/runtime-main.23d239d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/grocery-store/static/media/logo.5d5d9eef.svg"
+  }
+]);
